bdd13782c9f10e5bf496dfcd683ce5d18e890a30
wishu <wish@sek.ai>
2022-03-26 16:43:39 +1100

    correct naming in updateguilditems

--- network/mhfpacket/msg_mhf_update_guild_item.go
+++ network/mhfpacket/msg_mhf_update_guild_item.go
@@ -1,27 +1,27 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+ "errors"
 
  	"github.com/Solenataris/Erupe/network/clientctx"
 	"github.com/Solenataris/Erupe/network"
 	"github.com/Andoryuuta/byteframe"
 )
 
-type InsertedItem struct{
+type Item struct{
   Unk0 uint32
   ItemId uint16
-  NumItems uint16
+  Amount uint16
   Unk1 uint32
 }
 
 // MsgMhfUpdateGuildItem represents the MSG_MHF_UPDATE_GUILD_ITEM
 type MsgMhfUpdateGuildItem struct{
   AckHandle uint32
-  Unk0 uint32 // 0x00 0x00 0x00 0x03
-  NumItems uint16
+  GuildId uint32
+  Amount uint16
   Unk1 uint16 // 0x00 0x00
-  Items []InsertedItem
+  Items []Item // Array of updated item IDs
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -32,15 +32,15 @@ func (m *MsgMhfUpdateGuildItem) Opcode() network.PacketID {
 // Parse parses the packet from binary
 func (m *MsgMhfUpdateGuildItem) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
   m.AckHandle = bf.ReadUint32()
-  m.Unk0 = bf.ReadUint32()
-  m.NumItems = bf.ReadUint16()
+  m.GuildId = bf.ReadUint32()
+  m.Amount = bf.ReadUint16()
   m.Unk1 = bf.ReadUint16()
-  m.Items = make([]InsertedItem, int(m.NumItems))
+  m.Items = make([]Item, int(m.Amount))
 
-  for i := 0; i < int(m.NumItems); i++ {
+  for i := 0; i < int(m.Amount); i++ {
     m.Items[i].Unk0 = bf.ReadUint32()
     m.Items[i].ItemId = bf.ReadUint16()
-    m.Items[i].NumItems = bf.ReadUint16()
+    m.Items[i].Amount = bf.ReadUint16()
     m.Items[i].Unk1 = bf.ReadUint32()
   }
 
