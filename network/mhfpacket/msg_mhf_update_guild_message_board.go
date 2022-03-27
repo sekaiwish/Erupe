adeb657e16b504e34775e760495e33ff4e642389
wishu <wish@sek.ai>
2022-03-28 09:25:05 +1100

    parse updateguildmessageboard

--- network/mhfpacket/msg_mhf_update_guild_message_board.go
+++ network/mhfpacket/msg_mhf_update_guild_message_board.go
@@ -3,7 +3,8 @@ package mhfpacket
 import (
  "errors"
  "encoding/hex"
- "log"
+ "io"
+ 
  	"github.com/Solenataris/Erupe/network/clientctx"
 	"github.com/Solenataris/Erupe/network"
 	"github.com/Andoryuuta/byteframe"
@@ -12,19 +13,8 @@ import (
 // MsgMhfUpdateGuildMessageBoard represents the MSG_MHF_UPDATE_GUILD_MESSAGE_BOARD
 type MsgMhfUpdateGuildMessageBoard struct {
 	AckHandle uint32
-  // known opcodes:
-  // 0 => create message
-  // 1 => delete message (check guild leader?)
-  // 2 => update message (check author matches?)
-  // 3 =>
-  // 4 => like message?
   MessageOp uint32
-  PostType uint32 // 0 = message, 1 = news
-  StampId uint32
-  TitleLength uint32
-  BodyLength uint32
-  Title []byte
-  Body []byte
+  Request []byte
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -34,17 +24,10 @@ func (m *MsgMhfUpdateGuildMessageBoard) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfUpdateGuildMessageBoard) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
-  log.Println("ackhandle>", m.AckHandle)
-  log.Println(hex.Dump(bf.Data()))
-
   m.AckHandle = bf.ReadUint32()
   m.MessageOp = bf.ReadUint32()
-  m.PostType = bf.ReadUint32()
-  m.StampId = bf.ReadUint32()
-  m.TitleLength = bf.ReadUint32()
-  m.BodyLength = bf.ReadUint32()
-  m.Title = bf.ReadBytes(uint(m.TitleLength))
-  m.Body = bf.ReadBytes(uint(m.BodyLength))
+  m.Request = bf.DataFromCurrent()
+  bf.Seek(int64(len(bf.Data()) - 2), io.SeekStart)
 	return nil
 }
 
