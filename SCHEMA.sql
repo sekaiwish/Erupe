5f45769e26c2e135546bb077b18c46abdecceb52
wishu <wish@sek.ai>
2022-03-28 13:33:21 +1100

    add schema file

---
+++ SCHEMA.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+ALTER TABLE guilds
+    ADD COLUMN item_box bytea;
+
+CREATE TABLE guild_posts
+(
+    id serial NOT NULL PRIMARY KEY,
+    guild_id int NOT NULL,
+    author_id int NOT NULL,
+    post_type int NOT NULL,
+    stamp_id int NOT NULL,
+    post text NOT NULL,
+    likes int NOT NULL DEFAULT 0,
+    created_at int NOT NULL DEFAULT CAST(EXTRACT(epoch FROM now()) AS int)
+);
+
+END;
