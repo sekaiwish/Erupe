51b5fd659fc68e4d6004c767700634dab0b7f351
wishu <wish@sek.ai>
2022-04-28 08:13:03 +1000

    fix parsing on get_achievement

--- network/mhfpacket/msg_mhf_get_achievement.go
+++ network/mhfpacket/msg_mhf_get_achievement.go
@@ -1,7 +1,7 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+ "errors"
 
  	"github.com/Solenataris/Erupe/network/clientctx"
 	"github.com/Solenataris/Erupe/network"
@@ -13,7 +13,6 @@ type MsgMhfGetAchievement struct{
 	AckHandle      uint32
 	Unk0           uint16 // id?
 	Unk1           uint32 // char?
-	Unk2           uint32 // pad?
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -24,9 +23,8 @@ func (m *MsgMhfGetAchievement) Opcode() network.PacketID {
 // Parse parses the packet from binary
 func (m *MsgMhfGetAchievement) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
 	m.AckHandle = bf.ReadUint32()
-	m.Unk0 = bf.ReadUint16()
+	m.Unk0 = bf.ReadUint32()
 	m.Unk1 = bf.ReadUint32()
-	m.Unk2 = bf.ReadUint32()
 	return nil
 }
 
