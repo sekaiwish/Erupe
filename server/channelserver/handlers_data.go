1528e14d5317e62d4d7629ebf27a9dc794fd69a4
wishu <wish@sek.ai>
2022-05-15 09:10:30 +1000

    FF series quest patch

--- server/channelserver/handlers_data.go
+++ server/channelserver/handlers_data.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/Andoryuuta/byteframe"
 	"github.com/Solenataris/Erupe/common/bfutil"
 	"github.com/Solenataris/Erupe/network/mhfpacket"
 	"github.com/Solenataris/Erupe/server/channelserver/compression/deltacomp"
@@ -195,7 +196,10 @@ func handleMsgMhfLoaddata(s *Session, p mhfpacket.MHFPacket) {
 
 func handleMsgMhfSaveScenarioData(s *Session, p mhfpacket.MHFPacket) {
 	pkt := p.(*mhfpacket.MsgMhfSaveScenarioData)
-
+	_, err := s.server.db.Exec("UPDATE characters SET scenariodata = $1 WHERE characters.id = $2", pkt.RawDataPayload, int(s.charID))
+  if err != nil {
+    s.logger.Fatal("Failed to update scenario data in db", zap.Error(err))
+  }
 	// Do this ack manually because it uses a non-(0|1) error code
 	s.QueueSendMHF(&mhfpacket.MsgSysAck{
 		AckHandle:        pkt.AckHandle,
@@ -207,7 +211,19 @@ func handleMsgMhfSaveScenarioData(s *Session, p mhfpacket.MHFPacket) {
 
 func handleMsgMhfLoadScenarioData(s *Session, p mhfpacket.MHFPacket) {
 	pkt := p.(*mhfpacket.MsgMhfLoadScenarioData)
-	doAckBufSucceed(s, pkt.AckHandle, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	var scenarioData []byte
+	bf := byteframe.NewByteFrame()
+	err := s.server.db.QueryRow("SELECT scenariodata FROM characters WHERE characters.id = $1", int(s.charID)).Scan(&scenarioData)
+	if err != nil {
+		s.logger.Fatal("Failed to get scenario data contents in db", zap.Error(err))
+	} else {
+		if len(scenarioData) == 0 {
+			bf.WriteUint32(0x00)
+		} else {
+			bf.WriteBytes(scenarioData)
+		}
+	}
+	doAckBufSucceed(s, pkt.AckHandle, bf.Data())
 }
 
 func handleMsgMhfGetPaperData(s *Session, p mhfpacket.MHFPacket) {
